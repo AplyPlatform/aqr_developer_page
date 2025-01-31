dbd9b2e46ed5e54f1bfd497f1b73351e537e264c
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2025-01-31 15:49:50 +0900

    AAPI 적용

--- assets/js/custom/custom.js
+++ assets/js/custom/custom.js
@@ -18,37 +18,6 @@
         $('.page-loader').delay(1500).fadeOut(800);
     }
 
-    const targetsInfo = [
-        ["aply", "https://aply.biz/"],
-        ["aqr", "https://aqr.aplx.link"],
-        ["recruit", "https://home.aply.biz/recruit/index.html"],
-        ["duni", "https://duni.io"],
-        ["dunipilot", "https://pilot.duni.io"],
-        ["dunistock", "https://dunistock.com"],
-        ["dromi", "https://dromi.aply.biz"],
-        ["aplx", "https://aplx.aply.biz"],
-        ["dkdk", "https://dkdk.io"],
-        ["drdr", "https://drdr.io"],
-        ["blog", "https://blog.naver.com/PostList.naver?blogId=jebo78&categoryNo=38&from=postList"],
-        ["catchme", "https://catchme.aply.biz"],
-        ["kiosk", "https://kiosk.aply.biz"],
-        ["polaris", "https://polarisconsulting.modoo.at"]
-    ];
-
-    $('#selSites').change(function () {
-        let targetVal = $(this).val();
-        let targetUrl = "";
-
-        targetsInfo.forEach(function (t) {
-            if (t[0] == targetVal) {
-                targetUrl = t[1];
-            }
-        });
-
-        if (targetUrl != "")
-            window.open(targetUrl, "new");
-    });
-
     function chageLangSelect() {
         var langSelect = document.getElementById("id-lang");
 
